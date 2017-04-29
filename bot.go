a792ac7ef7bb09c59351a8f7ad5e8f64ce80d472
indiependente <rockerg991@gmail.com>
2017-04-29 21:25:28 +0100

    working in local

--- bot.go
+++ bot.go
@@ -3,9 +3,8 @@ package main
 import (
     "encoding/json"
     "net/http"
+    "bytes"
     "strings"
-    "strconv"
-    "log"
     "time"
 )
 
@@ -13,7 +12,68 @@ type currencyExchangeProvider interface {
 	rate(currencySymbol1 string, currencySymbol2 string) (float64, error)
 }
 
-func (c currencyExchangeProvider) rate(currencySymbol1 string, currencySymbol2 string) (float64, error) {
+type fixerio struct {}
+
+type multiCurrencyExchangeProvider []currencyExchangeProvider
+
+
+func main() {
+    
+    mcep := multiCurrencyExchangeProvider{
+        fixerio{},
+    }
+
+    http.HandleFunc("/rates/", func(w http.ResponseWriter, r *http.Request) {
+        begin := time.Now()
+        symbols := strings.SplitN(r.URL.Path, "/", 3)[2]
+        sym := strings.SplitN(symbols, "_", 2)
+        rate, err := mcep.rate(sym[0], sym[1])
+        
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+        json.NewEncoder(w).Encode(map[string]interface{}{
+            "rate": rate,
+            "took": time.Since(begin).String(),
+        })
+
+    })
+
+    http.ListenAndServe(":9090", nil)
+
+}
+
+func (mcep multiCurrencyExchangeProvider) rate(currencySymbol1 string, currencySymbol2 string) (float64, error){
+    rates := make(chan float64, len(mcep))
+    errs := make(chan error, len(mcep))
+
+    for _, provider := range mcep {
+        go func(cp currencyExchangeProvider) {
+            r, err := cp.rate(currencySymbol1, currencySymbol2)
+            if err != nil {
+                errs <- err
+                return
+            }
+            rates <- r
+        }(provider)
+    }
+
+    theRate := 0.0
+    for i := 0; i < len(mcep); i++ {
+        select {
+            case rate := <-rates:
+                theRate = rate
+            case err := <-errs:
+                return 0, err
+        }
+    }
+    return theRate, nil
+}
+
+func (c fixerio) rate(currencySymbol1 string, currencySymbol2 string) (float64, error) {
 	resp, err := http.Get("http://api.fixer.io/latest?base="+currencySymbol1+"&symbols="+currencySymbol2)
     if err != nil {
         return 0, err
@@ -21,17 +81,18 @@ func (c currencyExchangeProvider) rate(currencySymbol1 string, currencySymbol2 s
 
     defer resp.Body.Close()
 
-    var d struct {
-	Base string `json:"base"`
-	Date string `json:"date"`
-	Rates struct {
-		EUR float64 `json:"EUR"`
-	} `json:"rates"`
-}
+    var data map[string]interface{}
+    buf := new(bytes.Buffer)
+    buf.ReadFrom(resp.Body)
+    b := buf.Bytes()
 
-    if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-        return 0, err
+    if err := json.Unmarshal(b, &data); err != nil {
+        panic(err)
     }
-
-    return EUR, nil
+    if rates, ok := (data["rates"]).(map[string]interface{}); ok {
+        // fmt.Printf("%f\n", rates[currencySymbol2])
+        return rates[currencySymbol2].(float64), nil
+    } else {
+        panic(ok)
+    }
 }
